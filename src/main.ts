0c0ec5593ee54de2ec1e15a80ff858b6b3bc4e5d
MrJacobo1 <arieljacoboaquino@gmail.com>
2024-09-11 20:59:05 -0600

    Actividad 2 Miercoles 11 de septiembre

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
 //import './temas-ts/01-tipos-bas.ts'
 //import './temas-ts/02-interfaz-arre.ts'
-import './temas-ts/03-funciones_bas.ts'
+//import './temas-ts/04-destructuracion-obj-arre.ts'
+//import './temas-ts/03-funciones_bas.ts'
+import './temas-ts/05-destructuracion-funciones.ts'
+import './temas-ts/06-import-exports.ts'
+import './temas-ts/07-classe.ts'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
